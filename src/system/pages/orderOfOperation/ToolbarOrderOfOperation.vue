1ea6b465281e3eef54cfb275052f334b46ceaead
kanyesteves <kany.esteves@gmail.com>
2024-08-21 22:07:32 -0300

    Iniciado tela de OP.

---
+++ src/system/pages/orderOfOperation/ToolbarOrderOfOperation.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class=" card">
+    <Toolbar :class="$style.tolbarbox">
+      <template #start>
+        <Button size="small" label="Novo" raised @click="emit('onNewOp')" />
+        <Button :disabled="setVisibleToolbar.length != 1" :class="$style.buttons" size="small" label="Editar" severity="info" raised @click="emit('onEditOp')" />
+        <Button :disabled="setVisibleToolbar.length != 1" :class="$style.buttons" size="small" label="Remover" severity="danger" raised @click="emit('onRemoveOp')" />
+        <Button :disabled="setVisibleToolbar.length != 1" :class="$style.buttons" size="small" label="Fechar" severity="warn" raised @click="emit('onDoneOp')" />
+      </template>
+
+      <template #end>
+        <Button label="Exportar" icon="pi pi-download" size="small" />
+      </template>
+    </Toolbar>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineModel, defineEmits } from 'vue';
+import Button from 'primevue/button';
+import Toolbar from 'primevue/toolbar';
+
+const setVisibleToolbar = defineModel()
+const emit = defineEmits(['onNewOp', 'onEditOp', 'onRemoveOp', 'onDoneOp'])
+
+</script>
+
+<style module>
+  .tolbarbox {
+    margin-top: 1rem;
+  }
+
+  .buttons {
+    margin-left: 1rem;
+  }
+
+</style>
