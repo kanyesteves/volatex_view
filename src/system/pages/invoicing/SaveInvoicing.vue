58dd492b96a1e23bd159226b6dea2a1a09efb433
kanyesteves <kany.esteves@gmail.com>
2024-09-30 06:21:55 -0300

    Ajustando layout da tela de faturamento

--- src/system/pages/invoicing/SaveInvoicing.vue
+++ src/system/pages/invoicing/SaveInvoicing.vue
@@ -1,8 +1,6 @@
 <template>
   <Dialog v-model:visible="visible" modal header="Revisão do faturamento" :style="{ width: '60rem' }">
 
-    <h3></h3>
-
     <DataTable :value="records_for_invoice" showGridlines tableStyle="min-width: 50rem">
       <Column field="code_per_piece" header="Código"></Column>
       <Column field="invoiced" header="Faturado"></Column>
@@ -15,15 +13,38 @@
 
     <Divider />
 
-    <h3>Informações da Ordem de operação</h3>
     <div :class="$style.infos_of_op">
-      <label><b>Cliente:</b> {{ customer }}</label>
-      <label><b>Artigo:</b> {{ article }}</label>
-      <div v-for="wire of porcentage_wire" :key="wire.name">
-        <!-- <label><b>Fio:</b> {{ wire.name }}</label>
-        <label><b>Kg/Porcentagem:</b> {{ wire.por }}</label> -->
+      <div>
+        <InputGroup :style="{ 'max-width': '180px' }">
+          <InputGroupAddon>Cliente</InputGroupAddon>
+          <InputText v-model="customer" disabled />
+        </InputGroup>
+
+        <InputGroup :style="{ 'max-width': '230px' }">
+          <InputGroupAddon>Ordem de Operação</InputGroupAddon>
+          <InputText v-model="op.code" disabled />
+        </InputGroup>
+    
+        <InputGroup :style="{ 'max-width': '180px' }">
+          <InputGroupAddon>Artigo</InputGroupAddon>
+          <InputText v-model="article" disabled />
+        </InputGroup>
+
+        <InputGroup :style="{ 'max-width': '180px' }">
+          <InputGroupAddon>Peso total</InputGroupAddon>
+          <InputText v-model="total_weight" disabled />
+        </InputGroup>
+      </div>
+  
+      <div v-for="porcentage of porcentage_wire" :key="porcentage.name">
+        <InputGroup :style="{ 'max-width': '230px' }">
+          <InputGroupAddon>{{ porcentage.name }}</InputGroupAddon>
+          <InputText v-model="customer" disabled />
+          <InputGroupAddon>{{ porcentage.value + '%' }}</InputGroupAddon>
+        </InputGroup>
       </div>
     </div>
+
 
     <div :class="$style.space_bottons">
       <Button type="button" label="Cancelar" severity="secondary" @click="visible = false"></Button>
@@ -39,28 +60,38 @@ import Button from 'primevue/button';
 import Column from 'primevue/column';
 import Divider from 'primevue/divider';
 import DataTable from 'primevue/datatable';
+import InputText from 'primevue/inputtext';
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
 
 const visible = defineModel()
-const props = defineProps(['op', 'records_for_invoice'])
-
+const props = defineProps([
+  'op', 
+  'records_for_invoice'
+])
 const op = ref(props.op)
 const customer = ref('')
 const article = ref('')
 const porcentage_wire = ref([])
+const total_weight = ref(0.0)
+
 
 watch(() => props.op, (newValue) => {
   op.value = newValue
 
-  console.log(op.value.porcentage_wire)
   customer.value = op.value.customer.map(item => item.name).join(', ')
   article.value = op.value.article.map(item => item.name).join(', ')
-  porcentage_wire.value = op.value.porcentage_wire
+  porcentage_wire.value = op.value.wire_porcentage
 })
 
 const records_for_invoice = ref(props.records_for_invoice)
 watch(() => props.records_for_invoice, (newValue) => {
   records_for_invoice.value = newValue
 })
+
+const getValuesOfProduction = (records) => {
+  console.log(records)
+}
 
 const onSaveInvoincing = () => {
   console.log('Exportando...')
@@ -71,7 +102,6 @@ const onSaveInvoincing = () => {
 <style module>
   .infos_of_op {
     display: flex;
-    flex-direction: column;
   }
   .space_bottons {
     display: flex;
